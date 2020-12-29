858716aa7b8a81b2d1aa33d3ea6268f7c5f1559a
Toadless <thomasmcdonaldoctober@gmail.com>
2020-12-29 11:38:10 +0000

    database typo changes

--- src/main/java/me/toadless/Krisp/Database/Database.java
+++ src/main/java/me/toadless/Krisp/Database/Database.java
@@ -1,13 +1,14 @@
 package me.toadless.Krisp.Database;
 
 import com.mongodb.*;
+import me.toadless.Krisp.Utils.Config;
 
 public class Database {
     public static MongoClient mongoClient;
     public static DBCollection guild;
     public static DB database;
     public static void main() {
-        mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://toadless:ss77yWRX2za1DWkn@cluster0.hfsfa.mongodb.net"));
+        mongoClient = new MongoClient(new MongoClientURI(Config.database));
         database = mongoClient.getDB("Krisp");
         guild = database.getCollection("guilds");
     }
